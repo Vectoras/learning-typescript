0a8e582cb839976308c8d76eab22fa453591d2d0
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-01-28 10:12:12 +0000

    TS types

--- typescript.js
+++ typescript.js
@@ -1,5 +1,82 @@
+// first lines of TS
 var sum = function (a, b) {
     return a + b;
 };
 var answer = sum(2, 3);
 console.log(answer);
+// -- TYPES
+// boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = "brown";
+var favouriteQuote = "I'am not old, i'am only ".concat(age, ".");
+// Array
+var pets = ["cat", "dog", "pig"];
+var pets2 = ["lion", "dragon", "lizard"];
+// Object
+var wizard = {
+    a: "John"
+};
+// null and undefined
+var meh = undefined;
+var noo = null;
+// -- Types JS does not have but TS does allow for them
+// Tuple
+var basket;
+basket = ["basketball", 5];
+// Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size[2];
+console.log(sizeName);
+var sizeValue = Size.Small;
+console.log(sizeValue);
+// Any - !!! BE CAREFUL
+var whatever = "kasdlfj aslkdfj laksjdf";
+whatever = 5;
+whatever = Size.Small;
+// void
+var sing = function () { return console.log("lalalala"); };
+var sing2 = function () { return "lalalala"; };
+// never
+var error = function () {
+    // return "hello";
+    throw Error("oops");
+};
+var fightRobotArmy = function (robots) {
+    console.log("FIGHT!");
+};
+var fightRobotArmy2 = function (robots) {
+    console.log("FIGHT!");
+};
+var dog = {};
+dog.count;
+// Function
+var fightRobotArmy3 = function (robots) { return console.log("FIGHT!"); };
+// Classes
+var Animal = /** @class */ (function () {
+    function Animal(sound) {
+        this.sing = "alalalalala";
+        this.sing = sound;
+    }
+    Animal.prototype.greet = function () {
+        return "Hello ".concat(this.sing);
+    };
+    return Animal;
+}());
+var lion = new Animal("RAAAWWR");
+console.log(lion.greet());
+// console.log(lion.sing);
+// Union
+var confused = "hello";
+confused = 5;
+// confused = true;
+// type inference
+var x = 4;
+// x = "hello";
--- typescript.ts
+++ typescript.ts
@@ -1,3 +1,5 @@
+// first lines of TS
+
 const sum = (a: number, b: number) => {
   return a + b;
 };
@@ -5,3 +7,119 @@ const sum = (a: number, b: number) => {
 const answer = sum(2, 3);
 
 console.log(answer);
+
+// -- TYPES
+
+// boolean
+let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = "brown";
+let favouriteQuote: string = `I'am not old, i'am only ${age}.`;
+
+// Array
+let pets: string[] = ["cat", "dog", "pig"];
+let pets2: Array<string> = ["lion", "dragon", "lizard"];
+
+// Object
+let wizard: object = {
+  a: "John",
+};
+
+// null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
+
+// -- Types JS does not have but TS does allow for them
+
+// Tuple
+let basket: [string, number];
+basket = ["basketball", 5];
+
+// Enum
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
+let sizeName: String = Size[2];
+console.log(sizeName);
+let sizeValue: number = Size.Small;
+console.log(sizeValue);
+
+// Any - !!! BE CAREFUL
+let whatever: any = "kasdlfj aslkdfj laksjdf";
+whatever = 5;
+whatever = Size.Small;
+
+// void
+let sing = (): void => console.log("lalalala");
+let sing2 = (): string => "lalalala";
+
+// never
+let error = (): never => {
+  // return "hello";
+  throw Error("oops");
+};
+
+// interface (recomended) or type alias
+interface RobotArmy {
+  count: number;
+  type: string;
+  magic: string;
+}
+
+type RobotArmy2 = {
+  count: number;
+  type: string;
+  magic?: string; // ? - make the property optional
+};
+
+let fightRobotArmy = (robots: RobotArmy) => {
+  console.log("FIGHT!");
+};
+let fightRobotArmy2 = (robots: { count: number; type: string; magic: string }) => {
+  console.log("FIGHT!");
+};
+
+// Type Assertion
+interface CatArmy {
+  count: number;
+  type: string;
+  magic: string;
+}
+
+let dog = {} as CatArmy;
+dog.count;
+
+// Function
+let fightRobotArmy3 = (robots: RobotArmy): void => console.log("FIGHT!");
+
+// Classes
+class Animal {
+  private sing: string = "alalalalala";
+
+  constructor(sound: string) {
+    this.sing = sound;
+  }
+
+  greet(): string {
+    return `Hello ${this.sing}`;
+  }
+}
+
+let lion = new Animal("RAAAWWR");
+console.log(lion.greet());
+// console.log(lion.sing);
+
+// Union
+let confused: string | number = "hello";
+confused = 5;
+// confused = true;
+
+// type inference
+let x = 4;
+// x = "hello";
